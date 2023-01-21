397fc943b4f3ed0e032d808583b3b81e2d34e7c9
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2023-01-21 13:54:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ICS2O-UNIT6-04-JS-QUERY-STRINGS
+
+[![Mr Coxall's Super Linter](https://github.com/Alex-Nelson-1/ICS2O-UNIT6-04-JS-QUERY-STRINGS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Alex-Nelson-1/ICS2O-UNIT6-04-JS-QUERY-STRINGS/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Alex-Nelson-1/ICS2O-UNIT6-04-JS-QUERY-STRINGS)](https://repl.it/github/Alex-Nelson-1/ICS2O-UNIT6-04-JS-QUERY-STRINGS)
+
+This site can be found at: [https://Alex-Nelson-1.github.io/ICS2O-UNIT6-04-JS-QUERY-STRINGS/](https://Alex-Nelson-1.github.io/ICS2O-UNIT6-04-JS-QUERY-STRINGS/)
---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Created by: Alex Nelson
+// Created on: Dec 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-UNIT6-04-JS-QUERY-STRINGS/sw.js", {
+    scope: "/ICS2O-UNIT6-04-JS-QUERY-STRINGS/",
+  })
+}
+
+
+
+/**
+ * This function displays an alert.
+ */
+window.onload = function() {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get("r")
+  console.log(radius)
+
+  // process
+  const volume = (4 / 3) * Math.PI * (radius ** 3)
+
+
+  // output
+  document.getElementById("dimension").innerHTML = "r = " + radius
+  document.getElementById("output").innerHTML = "Volume: " + volume + " cm³"
+}
